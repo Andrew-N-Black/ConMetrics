0cbf1b6876da8f1cdb9aa0006fb2510eef3e3e96
Andrew-N-Black <63075313+Andrew-N-Black@users.noreply.github.com>
2024-06-18 18:40:53 -0700

    Update load.R

--- R/load.R
+++ R/load.R
@@ -1,3 +1,5 @@
+library(readxl)
+library(ggplot2)
 
 LOAD <- read_excel("data/load.xlsx")
 ggplot(LOAD,aes(GROUP,BURDEN))+geom_boxplot(aes(fill=GROUP),notch = F)+theme_bw()+scale_fill_manual(values = custom_colors) +labs(x = "", y = "Load")+ theme( plot.title = element_text(size = 16, face = "bold"),axis.text = element_text(size = 12),axis.title = element_text(size = 14, face = "bold"),legend.title = element_blank(),legend.position = "none" )
