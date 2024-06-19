d4706b0565c3e6d12a411c901fb4e54df235b42b
Andrew-N-Black <63075313+Andrew-N-Black@users.noreply.github.com>
2024-06-18 19:10:45 -0700

    Update branch.lengths.R

--- R/branch.lengths.R
+++ R/branch.lengths.R
@@ -1,5 +1,5 @@
 library(readxl)
 library(ggplot2)
 
-len <- read_excel("length.xlsx")
+len <- read_excel("data/length.xlsx")
 ggplot(len,aes(Group,LEN))+geom_boxplot(aes(fill=Group),notch = F)+theme_bw()+scale_fill_manual(values = custom_colors) +labs(x = "", y = "Branch Length")+ theme(plot.title = element_text(size = 16, face = "bold"),axis.text = element_text(size = 12),axis.title = element_text(size = 14, face = "bold"),legend.title = element_blank(),legend.position = "none")
