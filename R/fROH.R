adb7d3f103feb9996267069a05c26b043826785d
Andrew-N-Black <63075313+Andrew-N-Black@users.noreply.github.com>
2024-06-18 19:11:10 -0700

    Update fROH.R

--- R/fROH.R
+++ R/fROH.R
@@ -1,4 +1,4 @@
 library(readxl)
 library(ggplot2)
-roh <- read_excel("ROH.xlsx")
+roh <- read_excel("data/ROH.xlsx")
 ggplot(roh,aes(Group,ROH))+geom_boxplot(aes(fill=Group),notch = F)+theme_bw()+scale_fill_manual(values = custom_colors)
